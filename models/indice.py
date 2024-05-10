38f054ce42b7de21c757ef745836a268cef2c25c
deivison de alcantara <deivison-alcantara@hotmail.com>
2024-05-10 09:56:16 -0300

    ajuste model indice-view

--- models/indice.py
+++ models/indice.py
@@ -15,12 +15,17 @@ class Indice(db.Model):
     idh_longevidade = Column(Numeric(precision=8, scale=3), nullable=False)
     idh_educacao = Column(Numeric(precision=8, scale=3), nullable=False)
 
+
 @add_schema
 class IndiceByMunicipio(db.Model):
+    __tablename__ = 'indice_municipio_view'
+    __table_args__ = {"schema": "indices"}
     
     ano = Column(Numeric, primary_key=True)
-    codmunicipio = Column(String, nullable=False)
+    codmunicipio = Column(Integer, primary_key=True, nullable=False)
+    nomemunicipio = Column(String, nullable=False)
     idh_educacao = Column(Numeric(precision=8, scale=3), nullable=False)
     idh_renda = Column(Numeric(precision=8, scale=3), nullable=False)
+    #conserve as chaves primarias para que a query seja executada com sucesso
 
     
